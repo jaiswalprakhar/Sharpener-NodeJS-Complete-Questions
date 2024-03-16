b24000993901d48ef2006b433dc6104ff72f82fc
Prakhar Jaiswal <jaiswalprakhar98@gmail.com>
2024-03-16 05:59:07 +0530

    writing-and-reading-from-a-file

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+const { error } = require('console');
 
 //Task 6(Understanding the basics) - 
 
@@ -9,7 +11,7 @@ const http = require('http');
 
 //Task 7(Request / Response) -
 
-const app = http.createServer((req, res) => {
+/*const app = http.createServer((req, res) => {
     const url = req.url;
 
     if(url === '/home')
@@ -40,4 +42,62 @@ const app = http.createServer((req, res) => {
 
 app.listen(4000, (req, res) => {
     console.log(`Hi Prakhar server started at port 4000`);
+})*/
+
+//Task 8(Writing and reading from a file) -
+
+const app = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if(url === "/")   {
+    fs.readFile('message.txt', (err, data) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log(`Reading data from file ${data}`);
+        res.write('<html>');
+        res.write('<head><title>Chat App</title></head>');
+        res.write(`<body><p id = "para">${data}</p><form action ="/message" method = "POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body>`);
+        res.write('</html>');
+        return res.end();
+        //Alternate methode for reading file -
+        //console.log(data.toString()); //This will give error when no file is present
+        /*console.log(data);
+        return res.end(`<html>
+            <head><title>Chat App</title></head>
+            <body><p id = "para">${data}</p><form action ="/message" method = "POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body>
+            </html>`   
+            );*/
+    });
+  }
+
+  if(url === "/message" && method === "POST")   {
+    const body = [];
+    req.on('data', chunk => {
+        console.log(chunk);
+        body.push(chunk);
+    });
+
+    req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        //console.log(parsedBody);
+        const message = parsedBody.split("=")[1];
+        fs.writeFile('message.txt', message, err => {
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        })
+    })
+  }
+
+  /*res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();*/
+})
+
+app.listen(4000, (req, res) => {
+    console.log(`Server started at port 4000`);
 })
